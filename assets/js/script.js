0dad5b6031952ae6199180456badb0078d42423c
bealearnscode <bealearnscode@gmail.com>
2020-02-17 01:40:09 -0500

    comment out all rules. probably not needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,66 @@
-// Modals
-const newCustomerModal = document.querySelector('#new-customer-modal');
-const returningCustomerModal = document.querySelector('#returning-customer-modal');
-const inventoryModal = document.querySelector('#inventory-modal');
+// // Modals
+// const newCustomerModal = document.querySelector('#new-customer-modal');
+// const returningCustomerModal = document.querySelector('#returning-customer-modal');
+// const inventoryModal = document.querySelector('#inventory-modal');
 
-// Modal Buttons
-const newCustomerModalBtn = document.querySelector('#new-customer-modal-btn');
-const returningCustomerModalBtn = document.querySelector('#returning-customer-modal-btn');
-const inventoryModalBtn = document.querySelector('#inventory-modal-btn');
-const closeNewCustomerBtn = document.querySelector('.closeNewCustomer');
-const closeReturningCustomerBtn = document.querySelector('.closeReturningCustomer');
-const closeInventoryBtn = document.querySelector('.closeInventory');
+// // Modal Buttons
+// const newCustomerModalBtn = document.querySelector('#new-customer-modal-btn');
+// const returningCustomerModalBtn = document.querySelector('#returning-customer-modal-btn');
+// const inventoryModalBtn = document.querySelector('#inventory-modal-btn');
+// const closeNewCustomerBtn = document.querySelector('.closeNewCustomer');
+// const closeReturningCustomerBtn = document.querySelector('.closeReturningCustomer');
+// const closeInventoryBtn = document.querySelector('.closeInventory');
 
-// Events
-newCustomerModalBtn.addEventListener('click', openNewCustomerModal);
-returningCustomerModalBtn.addEventListener('click', openReturningCustomerModal);
-inventoryModalBtn.addEventListener('click', openInventoryModal);
-closeNewCustomerBtn.addEventListener('click', closeNewCustomerModal);
-closeReturningCustomerBtn.addEventListener('click', closeReturningCustomerModal);
-closeInventoryBtn.addEventListener('click', closeInventoryModal);
-window.addEventListener('click', outsideClick);
+// // Events
+// newCustomerModalBtn.addEventListener('click', openNewCustomerModal);
+// returningCustomerModalBtn.addEventListener('click', openReturningCustomerModal);
+// inventoryModalBtn.addEventListener('click', openInventoryModal);
+// closeNewCustomerBtn.addEventListener('click', closeNewCustomerModal);
+// closeReturningCustomerBtn.addEventListener('click', closeReturningCustomerModal);
+// closeInventoryBtn.addEventListener('click', closeInventoryModal);
+// window.addEventListener('click', outsideClick);
 
-// Open Modals
-function openNewCustomerModal() 
-{
-    newCustomerModal.style.display = 'block';
-}
-function openReturningCustomerModal() 
-{
-    returningCustomerModal.style.display = 'block';
-}
-function openInventoryModal() 
-{
-    inventoryModal.style.display = 'block';
-}
+// // Open Modals
+// function openNewCustomerModal() 
+// {
+//     newCustomerModal.style.display = 'block';
+// }
+// function openReturningCustomerModal() 
+// {
+//     returningCustomerModal.style.display = 'block';
+// }
+// function openInventoryModal() 
+// {
+//     inventoryModal.style.display = 'block';
+// }
 
-// Close Modals
-function closeNewCustomerModal() 
-{
-  newCustomerModal.style.display = 'none';
-}
-function closeReturningCustomerModal() 
-{
-  returningCustomerModal.style.display = 'none';
-}
-function closeInventoryModal() 
-{
-  inventoryModal.style.display = 'none';
-}
+// // Close Modals
+// function closeNewCustomerModal() 
+// {
+//   newCustomerModal.style.display = 'none';
+// }
+// function closeReturningCustomerModal() 
+// {
+//   returningCustomerModal.style.display = 'none';
+// }
+// function closeInventoryModal() 
+// {
+//   inventoryModal.style.display = 'none';
+// }
 
-// Close If Outside Click
-function outsideClick(e) 
-{
-  if (e.target == newCustomerModal) 
-  {
-    newCustomerModal.style.display = 'none';
-  }
-   else if (e.target == returningCustomerModal) 
-   {
-     returningCustomerModal.style.display = 'none';
-   }
-   else if (e.target == inventoryModal) 
-   {
-     inventoryModal.style.display = 'none';
-   }
- }
+// // Close If Outside Click
+// function outsideClick(e) 
+// {
+//   if (e.target == newCustomerModal) 
+//   {
+//     newCustomerModal.style.display = 'none';
+//   }
+//    else if (e.target == returningCustomerModal) 
+//    {
+//      returningCustomerModal.style.display = 'none';
+//    }
+//    else if (e.target == inventoryModal) 
+//    {
+//      inventoryModal.style.display = 'none';
+//    }
+//  }
